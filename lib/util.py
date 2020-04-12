dc02f4334dfa8fab0b7f7532f42c33574e03d839
amet <amet.nospam@gmail.com>
2020-04-12 17:47:27 +0400

    revert: Update to Kodi v18 skin conditional visibility InfoLabels

--- lib/util.py
+++ lib/util.py
@@ -554,8 +554,7 @@ def getProgressImage(obj):
 
 
 def trackIsPlaying(track):
-    return xbmc.getCondVisibility('String.StartsWith(MusicPlayer.Comment,{0})'.format('PLEX-{0}:'.format(track.ratingKey)))
-
+    return xbmc.getCondVisibility('SubString(MusicPlayer.Comment,{0},Left)'.format('PLEX-{0}:'.format(track.ratingKey)))
 
 def addURLParams(url, params):
         if '?' in url:
